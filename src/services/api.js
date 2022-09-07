0691c6cab7d13841a391be86786c9a273cbc17d7
KayZinMaung1 <kayzinmg363@gmail.com>
2022-09-07 10:14:07 +0630

    change server

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const host = "http://localhost:3001";
+export const host = "https://evening-dawn-38302.herokuapp.com";
 export const call = async(method, path, data)=>{
     const response = await axios[method](`${host}/${path}`, data);
     return response;
